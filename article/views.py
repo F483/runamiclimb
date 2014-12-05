f1b871a69dc26da760b0ab1649432333574f1f7d
KVSocial <admin@techmarketing.cc>
2014-12-05 12:46:05 -0500

    adding homepage

--- article/views.py
+++ article/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import Article
 
 def homepage(request):
-	return HttpResponse("Hello, world.")
+	articles = Article.objects.all()
+	return render(request, 'article/homepage.html', {"articles" : articles })
+
