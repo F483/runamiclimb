079bd698c77a4f0d38504ab678015d4c1c9bff9b
techmarketing <admin@techmarketing.cc>
2014-12-07 13:00:03 -0500

    added bold to current issue and current category

--- article/views.py
+++ article/views.py
@@ -5,7 +5,8 @@ from article.models import Category
 from article.models import Issue
 from django.shortcuts import get_object_or_404
 
-def homepage(request):
+def homepage(request, year, month, categorytitle):
+#	currentissue = get_object_or_404(Issue, monthe=int(month), year=int(year))	
 	articles = Article.objects.filter(featured=True)
 	categories = Category.objects.all()
 	issues = Issue.objects.all()
@@ -17,5 +18,16 @@ def displayarticle(request, id):
 	article = get_object_or_404(Article, id=id)
 	categories = Category.objects.all()
 	issues = Issue.objects.all()
-	return render(request, 'article/article.html', {"article" : article, "categories" : categories, "issues" : issues})
+	currentissue = article.issue
+	currentcategory = article.category
+
+	templatearguments = {
+		"article" : article, 
+		"categories" : categories, 
+		"issues" : issues,
+		"currentcategory" : currentcategory,
+		"currentissue" : currentissue,
+	}
+	
+	return render(request, 'article/article.html', templatearguments)
 
