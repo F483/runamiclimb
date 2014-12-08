11cde3f89f79ba73600afe59f9873abd134de919
F483 <fabian.barkhau@gmail.com>
2014-12-08 18:52:17 +0100

    use slug for category urls

--- article/models.py
+++ article/models.py
@@ -1,4 +1,6 @@
+from unidecode import unidecode                                                                                 
 from django.db import models
+from django.template.defaultfilters import slugify                               
 
 # Create your models here.
 
@@ -12,19 +14,23 @@ class Article(models.Model):
   category = models.ForeignKey("article.Category", related_name="articles", null=True)
   featured = models.BooleanField(default=False)
   issue = models.ForeignKey("article.Issue", related_name="articles", null=True)
+
   def url(self):
-  	return "/article/%s/%s.html" % (self.id, "todo-title-slug")
+    return "/article/%s/%s.html" % (self.id, "todo-title-slug")
 
   def __unicode__(self):
-  	return self.title
-
+    return self.title
 
 
 class Category(models.Model):
 
   title = models.CharField(max_length=1024)
+
+  def slug(self):
+    return slugify(unidecode(self.title)) 
+
   def __unicode__(self):
-  	return self.title
+    return self.title
 
 
 class Issue(models.Model):
@@ -34,7 +40,7 @@ class Issue(models.Model):
 
   def __unicode__(self):
     return "%i - %i" % (self.year, self.month)
-  	
+    
   class Meta:
 
     ordering = ["-year", "-month"]
--- article/views.py
+++ article/views.py
@@ -4,54 +4,61 @@ from article.models import Article
 from article.models import Category
 from article.models import Issue
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 def homepage(request):
-    return listarticles(request, None, None, None)
+  return listarticles(request, None, None, None)
 
 def issue(request, year, month):
-    return listarticles(request, None, year, month)
+  return listarticles(request, None, year, month)
 
-def listarticles(request, category_title, year, month):
-    articles = Article.objects.all()
+def listarticles(request, category_slug, year, month):
+  import pudb; pu.db
+  articles = Article.objects.all()
+  categories = Category.objects.all()
+  issues = Issue.objects.all()
 
-    currentcategory = None
-    if not category_title:
-        articles = articles.filter(featured=True)
-    else:
-        currentcategory = get_object_or_404(Category, title=category_title)
-        articles = articles.filter(category=currentcategory)
+  currentcategory = None
+  if not category_slug:
+    articles = articles.filter(featured=True)
+  else:
+    for category in categories:
+      slug = category.slug()
+      if slug == category_slug:
+        currentcategory == category
+    if not currentcategory:
+      raise Exception("test")
+      #raise Http404
+    articles = articles.filter(category=currentcategory)
 
-    currentissue = None
-    if month and year:
-        currentissue = get_object_or_404(Issue, month=int(month), year=int(year))
-    else:
-        currentissue = Issue.objects.all()[0]
-    articles = articles.filter(issue=currentissue)
+  currentissue = None
+  if month and year:
+    currentissue = get_object_or_404(Issue, month=int(month), year=int(year))
+  else:
+    currentissue = Issue.objects.all()[0]
+  articles = articles.filter(issue=currentissue)
 
-    categories = Category.objects.all()
-    issues = Issue.objects.all()
-    templatearguments = {
-        "articles" : articles, 
-        "categories" : categories, 
-        "issues" : issues,
-        "currentcategory" : currentcategory,
-        "currentissue" : currentissue,
-    }
-    return render(request, 'article/homepage.html', templatearguments)
-
+  templatearguments = {
+    "articles" : articles,
+    "categories" : categories,
+    "issues" : issues,
+    "currentcategory" : currentcategory,
+    "currentissue" : currentissue,
+  }
+  return render(request, 'article/homepage.html', templatearguments)
 
 def displayarticle(request, id):
-    article = get_object_or_404(Article, id=id)
-    categories = Category.objects.all()
-    issues = Issue.objects.all()
-    currentissue = article.issue
-    currentcategory = article.category
-    templatearguments = {
-        "article" : article, 
-        "categories" : categories, 
-        "issues" : issues,
-        "currentcategory" : currentcategory,
-        "currentissue" : currentissue,
-    }
-    return render(request, 'article/article.html', templatearguments)
+  article = get_object_or_404(Article, id=id)
+  categories = Category.objects.all()
+  issues = Issue.objects.all()
+  currentissue = article.issue
+  currentcategory = article.category
+  templatearguments = {
+    "article" : article,
+    "categories" : categories,
+    "issues" : issues,
+    "currentcategory" : currentcategory,
+    "currentissue" : currentissue,
+  }
+  return render(request, 'article/article.html', templatearguments)
 
