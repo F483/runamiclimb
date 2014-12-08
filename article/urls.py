a5c4cd798bc5e788011f5f2a9227c5f77d2ea71d
F483 <fabian.barkhau@gmail.com>
2014-12-08 16:57:05 +0100

    add article category

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('article.views',
 	url(r"^$", "homepage"),
+  url(r"^articles/(?P<category_title>[a-z]+)/(?P<year>[0-9]+)\-(?P<month>[0-9]+)/", "listarticles"),
 	url(r"^article/(?P<id>[0-9]+)/[a-z0-9\-]+\.html$", "displayarticle"),
 )
