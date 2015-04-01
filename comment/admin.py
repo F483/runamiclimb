4c937dfbccf9c500bf07680c08237a9f8e05251c
F483 <fabian.barkhau@gmail.com>
2015-04-01 20:19:44 +0200

    added relation column to comment admin view

--- comment/admin.py
+++ comment/admin.py
@@ -3,7 +3,14 @@ from comment.models import Comment
 
 class CommentAdmin(admin.ModelAdmin):
 
-  list_display = ('alias', 'created')
+  list_display = ('alias', 'created', 'relation')
+
+  def relation(self, obj):
+    article = obj.article_comments.first()
+    if article:
+      return "<a href='%s'>%s</a>" % (article.url(), article.title)
+    return ""
+  relation.allow_tags = True
 
 admin.site.register(Comment, CommentAdmin)
 
